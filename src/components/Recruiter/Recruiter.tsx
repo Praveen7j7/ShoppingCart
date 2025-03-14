f4f542ebe0432decb61f8eaeac4447ff75c6df2e
Praveen Jha <praveenworkforum@gmail.com>
2025-03-14 11:05:26 +0530

    firstP

--- src/components/Recruiter/Recruiter.tsx
+++ src/components/Recruiter/Recruiter.tsx
@@ -1,23 +1,25 @@
-import netherlandsRecruiter from './jeremy-akeze-doghouse-it-recruitment.jpg';
+import netherlandsRecruiter from './pundrika-acarya.jpg';
 import * as S from './style';
 
 const Recruiter = () => (
   <S.Container>
     <S.Thumbnail>
       <img
-        alt="Jeremy Akeze - Doghouse IT Recruitment"
+        alt="Praveen Jha IT Recruitment"
         src={netherlandsRecruiter}
       />
     </S.Thumbnail>
     <S.Description>
       <h4>
-        Work in the Netherlands
+        Work in the India
         <S.Flag />
       </h4>
       <p>
-        Hi! I'm Jeremy Akeze from Doghouse IT Recruitment and I'm looking for
-        skilled Software Engineers like you. If you wish to move abroad,{' '}
-        <a href="https://www.linkedin.com/in/jeremy-akeze-9542b396/">
+
+Hi! I'm Praveen Jha from IT Recruitment, and I'm looking for skilled Software Engineers like you. 
+{' '}
+        <a href="https://www.linkedin.com/in/praveenjha07/">
+        <br />
           <b>follow me on Linkedin.</b>
         </a>
       </p>
